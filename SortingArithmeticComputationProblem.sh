d29a0d236804e8763ab7b6f6dbedcd6f308d8c38
hhmadan <hhmadan03@gmail.com>
2022-09-22 19:11:47 +0530

    Added: UC8_SortResultsInDescendingOrder

--- SortingArithmeticComputationProblem.sh
+++ SortingArithmeticComputationProblem.sh
@@ -2,31 +2,55 @@ read -p "Enter 1st number: " a
 read -p "Enter 2nd number: " b
 read -p "Enter 3rd number: " c
 echo "Three numbers are: $a $b $c"
-
-echo "First Compute: $a + $b * $c = $(( a + b * c ))"
-echo "Second Compute: $a * $b + $c = $(( a * b + c ))"
-echo "Third Compute: $c + $a / $b = $(( c + a / b ))"
-echo "Fourth Compute: $a % $b + $c = $(( a % b + c ))"
+echo "$a + $b * $c = $(( a + b * c ))"
+echo "$a * $b + $c = $(( a * b + c ))"
+echo "$c + $a / $b = $(( c + a / b ))"
+echo "$a % $b + $c = $(( a % b + c ))"
 
 first_computation=$(( a + b * c ))
 second_computation=$(( a * b + c ))
 third_computation=$(( c + a / b ))
 fourth_computation=$(( a % b + c ))
+echo "$a + $b * $c = $first_computation"
+echo "$a * $b + $c = $second_computation"
+echo "$c + $a / $b = $third_computation"
+echo "$a % $b + $c = $fourth_computation"
 
 declare -A dictionary
+declare -a array
 
 dictionary[first_computation]=$first_computation
 dictionary[second_computation]=$second_computation
 dictionary[third_computation]=$third_computation
 dictionary[fourth_computation]=$fourth_computation
 
-declare -a array
 index=0
-
 for computation in ${!dictionary[@]}
 do 
     echo "$computation = ${dictionary[$computation]}"
     array[index++]=${dictionary[$computation]}
 done
+echo ${array[@]}
 
-echo "Array is: ${array[@]}"
+size=${#array[@]}
+for(( i = 0; i < size - 1; i++ ))
+do
+    swapped=0
+    for(( j = 0; j < size - i - 1; j++))
+    do
+        if((array[j] < array[j+1]))
+        then
+            temp=${array[j]}
+            array[j]=${array[j+1]}
+            array[j+1]=$temp
+            swapped=1
+        fi
+    done
+    if((swapped==0))
+    then
+        break
+    fi
+done
+
+echo "Results sorted in descending order: "
+echo ${array[@]}
